9cb568a70880496e61fb21bbe0780e13f12628cc
ScubeSGraham <StephenGraham@scubeenterprise.com>
2023-07-24 12:43:40 -0400

    commit code for the day

---
+++ ScubeMarbleChallenge/Marble.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScubeMarbleChallenge
+{
+    public class Marble
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public Double Weight { get; set; }
+
+        public override String ToString()
+        {
+            // { id: 1, color: "blue", name: "Bob", weight: 0.5 },
+            return "{ id: " + Id + ", color: " + Color + ", name: " + Name + ", weight: " + Weight + " }";
+        }
+    }
+}
--- ScubeMarbleChallenge/Program.cs
+++ ScubeMarbleChallenge/Program.cs
@@ -1,2 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ScubeMarbleChallenge;
+using System.Text;
+
+String command = "";
+
+while (command != "exit")
+{
+    Console.WriteLine("Enter a command");
+    command = Console.ReadLine().ToLower();
+    Console.WriteLine("You entered: " + command);
+
+    if (command == "testsetup")
+    {
+        // Do something
+    }
+    else if (command == "testmarble")
+    {
+        Marble marble = new Marble();
+        marble.Id = 1;
+        marble.Name = "Bob";
+        marble.Color = "blue";
+        marble.Weight = 0.5;
+
+        Console.WriteLine(marble);
+    }
+    else if (command == "testclean")
+    {
+        String testString = "Bob o’Bob";
+        String cleanString = testString.ToLower();
+        char[] testArray = testString.ToCharArray();
+        StringBuilder builder = new StringBuilder();
+
+        foreach (char testChar in testArray)
+        {
+            if (char.IsLetterOrDigit(testChar))
+            {
+                builder.Append(testChar);
+            }
+        }
+        cleanString = builder.ToString();
+
+        Console.WriteLine("Input: " + testString + ", Output: " + cleanString);
+    }
+    else if (command == "testpalindrome")
+    {
+        String testString = "bob";
+        char[] testArray = testString.ToCharArray();
+        Array.Reverse(testArray);
+        String reverseString = new String(testArray);
+
+        bool isPalindrome = false;
+        if (testString == reverseString)
+        {
+            isPalindrome = true;
+        }
+
+        Console.WriteLine("Input: " + testString + ", Output: " + reverseString + ", Palindrome: " + isPalindrome);
+    }
+    else if (command == "testfilter")
+    {
+        List<Marble> marbleList = new List<Marble>()
+        {
+            new Marble() {Id = 1, Color = "blue", Weight = 0.5, Name = "bob"},
+            new Marble() {Id = 2, Color = "red", Weight = 0.4, Name = "fred"},
+            new Marble() {Id = 3, Color = "violet", Weight = 0.6, Name = "sam"}
+        };
+
+        foreach (Marble marble in marbleList)
+        {
+            Console.WriteLine(marble);
+        }
+
+        // LEFT OFF HERE
+    }
+    else if (command != "exit")
+    {
+        Console.WriteLine("Unknown command, try again");
+    }
+}
